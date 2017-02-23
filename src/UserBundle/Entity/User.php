e213fec66c9ac4cda979986b8916b0343d8ee95f
quentin <quentin.buirette@hotmail.fr>
2017-02-23 11:05:48 +0100

    feat: add user model

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -2,7 +2,148 @@
 
 namespace UserBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
 class User
 {
+    /**
+    * @var integer
+    *
+    * @ORM\Column(name="id", type="integer")
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    */
+    private $id;
 
+    /**
+    * @var string
+    *
+    * @ORM\Column(name="name", type="string", length="150")
+    */
+    private $name;
+
+    /**
+    * @var string
+    *
+    * @ORM\Column(name="email", type="string", length="100")
+    */
+    private $email;
+
+    /**
+    * @var string
+    *
+    * @ORM\Column(name="password", type="string", length="100")
+    */
+    private $password;
+
+    /**
+    * @var string
+    *
+    * @ORM\Column(name="role", type="string", length="100")
+    */
+    private $role;
+
+    /**
+    * Get id
+    *
+    * @return integer 
+    */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+    * Get name
+    *
+    * @return string 
+    */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+    * Set name
+    *
+    * @param string $name
+    * @return User
+    */
+    public function setSlug($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+    * Get email
+    *
+    * @return User
+    */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+    * Set email
+    *
+    * @param string $email
+    * @return User
+    */
+    public function setEmail()
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+    * Get password
+    *
+    * @return User
+    */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+    * Set password
+    *
+    * @param string $password
+    * @return User
+    */
+    public function setPassword()
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+    * Get role
+    *
+    * @return User
+    */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+    * Set role
+    *
+    * @param string $role
+    * @return User
+    */
+    public function setRole()
+    {
+        $this->role = $role;
+        return $this;
+    }
 }
